e7392c2021eeae8a7d49b8effb1f0f1f007f3e10
danielbatres <danielbatresuser@gmail.com>
2023-10-12 19:51:46 -0600

    Drawer navigation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StyleSheet } from 'react-native';
+import "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { NavigationTab } from './src/navigation/NavigationTab';
+import { NavigationDrawer } from "./src/navigation/NavigationDrawer";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NavigationTab />
+      <NavigationDrawer />
     </NavigationContainer>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
